5c348b472b917b1fdc5c94a40e6ca9368a66ae1e
samirwebdev <samirwebdev@duck.com>
2025-08-31 13:06:15 +0530

    Javascript fixed

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-const emoji = document.getElementById("emoji");
-const name = document.getElementById("name");
 const url =
 	"https://emoji-api.com/emojis?access_key=307afbaa4544c6f35098b36ba0907c9edeb3f0f5";
-const btn = document.getElementById("btn");
-let fall = document.querySelector(".fall");
-
-const storeData = data => {
-	data = JSON.stringify(data);
-	localStorage.setItem("Emoji", data);
-};
-
-const getData = async () => {
+const emoji = document.querySelector("#emoji");
+const name = document.querySelector("#name");
+const btn = document.querySelector("#btn");
+const falls = document.querySelectorAll(".fall");
+const loadEmoji = async () => {
 	try {
 		let res = await fetch(url);
 		let data = await res.json();
-		storeData(data);
+		data = JSON.stringify(data);
+		localStorage.setItem("Emojis", data);
 	} catch (e) {
-		alert("Something went wrong! Please refresh the page!");
+		name.innerText = "Something went wrong! Please try again.";
 	}
 };
-const showData = (eData, nData) => {
-	emoji.innerText = eData;
-	name.innerText = nData;
-};
-const retriveData = () => {
-	if (localStorage.getItem("Emoji")) {
-		let data = localStorage.getItem("Emoji");
-		data = JSON.parse(data);
-		return data;
+const showEmoji = () => {
+	if (!localStorage.getItem("Emojis")) {
+		loadEmoji();
 	}
-	else{
-	  getData();
-	}
+	name.innerText = "Loading...";
+	let n = Math.floor(Math.random() * 1859);
+	let = emojiArray = localStorage.getItem("Emojis");
+	emojiArray = JSON.parse(emojiArray);
+	emoji.innerText = emojiArray[n].character;
+	name.innerText = emojiArray[n].unicodeName;
 };
-const main = () => {
-	let data = retriveData();
-	let num = Math.floor(Math.random() * 1859);
-	showData(data[num].character, data[num].unicodeName);
-};
+
+if (!localStorage.getItem("Emojis")) {
+	document.addEventListener("DOMContentLoaded", loadEmoji);
+} else {
+	name.innerText = "Go on buddy! Everything is Ready.";
+}
+btn.addEventListener("click", showEmoji);
+
 const falling = () => {
-	let num = Math.floor(Math.random() * 1859);
-	let data = retriveData();
+	let n = Math.floor(Math.random() * 1859);
 	let r = Math.floor(Math.random() * 80);
-	fall.innerText = data[num].character;
-	fall.style.left = r + "vw";
+	let emojiArray = localStorage.getItem("Emojis");
+	emojiArray = JSON.parse(emojiArray);
+	falls.forEach(fall => {
+		fall.innerText = emojiArray[n].character;
+		fall.style.left = r + "vw";
+	});
+	console.log(emojiArray);
 };
-main();
 falling();
 setInterval(falling, 5000);
-btn.addEventListener("click", main);
